9511730e33ff4d8e3ddf03992f6ea2a51c5adc1a
alicanyilmazz <alicanyilmaz101@gmail.com>
2021-09-30 22:14:10 +0300

    Delegate Design Pattern!

--- DelegatePractice/main.swift
+++ DelegatePractice/main.swift
@@ -1,11 +1,64 @@
-//
-//  main.swift
-//  DelegatePractice
-//
-//  Created by alican on 30.09.2021.
-//
+protocol AdvancedLifeSupport{
+    func performCPR()
+}
 
-import Foundation
+class EmergencyCallHandler{
+    var delegate : AdvancedLifeSupport?
+    
+    func assessSituation(){
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency(){
+        delegate?.performCPR()
+    }
+}
 
-print("Hello, World!")
+struct Paramedic : AdvancedLifeSupport{
+    
+    init(handler : EmergencyCallHandler){
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compression, 30 per second.")
+    }
+}
+
+class Doctor : AdvancedLifeSupport{
+    
+    init(handler : EmergencyCallHandler){
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compression, 90 per second.")
+    }
+    
+    func useStethescope(){
+        print("Listening for heart sounds.")
+    }
+    
+}
+
+class Surgeon : Doctor{
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeeGees")
+    }
+    
+    func useElectricDrill(){
+        print("Whirr...")
+    }
+    
+}
+
+
+let _handler = EmergencyCallHandler()
+let _paramedic = Paramedic(handler: _handler)
+
+_handler.assessSituation()
+_handler.medicalEmergency()
+
+let _surgeon = Surgeon(handler: _handler)
 
